2db2204de6f67271200a04705f23f7e00cc7f1e0
Rohit-Dhade <rohitdhade99@gmail.com>
2026-02-21 17:19:26 +0530

    Cors added.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cookieparser = require("cookie-parser");
+const cors = require('cors')
 const app = express();
 const authRouter = require("./routes/auth.routes");
 const postRouter = require("./routes/post.routes");
 const userRouter = require("./routes/user.routes");
 app.use(express.json());
 app.use(cookieparser());
+app.use(cors());
 
 app.use("/api/auth", authRouter);
 app.use("/api/post", postRouter);
